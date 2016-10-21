23cdf9c0c379e0f6fdcd351259611243be81e49e
Florian Delezenne <del.florian@gmail.com>
2016-10-21 15:17:55 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  context: __dirname,
+
+  entry: [
+    'webpack-dev-server/client?http://127.0.0.1:8080',
+    './src/index.jsx',
+  ],
+
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public'),
+  },
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+
+  devServer: {
+    contentBase: './public',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
+
+module.exports = config;
