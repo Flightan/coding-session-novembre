6a222741a97bceeb71262456af1e5edd0134cccd
Roman Quelen <rquelen@octo.com>
2016-11-25 19:35:41 +0100

    add leaflet marker

--- src/LeafletMap/LeafletMap.jsx
+++ src/LeafletMap/LeafletMap.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 import { container } from './LeafletMap.css';
 
 const LeafletMap = ({ lat, long, zoom }) =>
@@ -8,6 +8,7 @@ const LeafletMap = ({ lat, long, zoom }) =>
       url="http://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoibmlja2kiLCJhIjoiczVvbFlXQSJ9.1Gegt3V_MTupW6wfjxq2QA"
       attribution='&copy; <a href="https://www.mapbox.com/about/maps/" target="_blank">Mapbox</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
     />
+    <Marker position={[lat, long]} />
   </Map>;
 
 LeafletMap.propTypes = {
