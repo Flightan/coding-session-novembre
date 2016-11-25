3ddcafbf5c6154714977f343cf872bb11f9c041d
Florian Delezenne <del.florian@gmail.com>
2016-11-25 12:08:15 +0100

    First test with Enzyme

--- src/App/App.spec.jsx
+++ src/App/App.spec.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App component', () => {
+  it('should display Hello world', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.html()).to.contain('Hello world');
+  });
 });
